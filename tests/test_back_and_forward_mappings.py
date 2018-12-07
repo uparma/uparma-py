8c60f60548e82b5d4c583deba3e3cd63d752ca77
Christian Fufezan <christian@fufezan.net>
2018-12-07 17:43:24 +0100

    Adjusted test so it won't break if only one style is defined in an entry in parameters

--- tests/test_back_and_forward_mappings.py
+++ tests/test_back_and_forward_mappings.py
@@ -7,17 +7,21 @@ up = uparma.UParma(refresh_jsons=True)
 
 
 def test_simple_back_and_forward_mapping():
-    skey = random.choice(list(up.parameters.keys()))
+    while True:
+        skey = random.choice(list(up.parameters.keys()))
 
-    param_dict = up.parameters[skey]
+        param_dict = up.parameters[skey]
+        available_styles = [k for k in param_dict.keys() if 'style' in k]
+        if len(available_styles) >= 2:
+            break
 
     while True:
         # must be sure to have both keys in the random param_dict
         try:
-            source_style, target_style = random.choices(up.available_styles, k=2)
+            source_style, target_style = random.choices(available_styles, k=2)
             # python 3.7
         except:
-            source_style, target_style = random.sample(up.available_styles, 2)
+            source_style, target_style = random.sample(available_styles, 2)
             # python <3.7
         if source_style in param_dict.keys() and target_style in param_dict.keys():
             if isinstance(param_dict[source_style], str) and \
