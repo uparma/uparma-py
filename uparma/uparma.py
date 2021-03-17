ca40db2f31248badc8f7a9fe24d3bfe2ad1df6fd
Christian Fufezan <christian@fufezan.net>
2021-03-17 10:31:06 +0100

    Add style argument for consistency

--- uparma/uparma.py
+++ uparma/uparma.py
@@ -406,11 +406,11 @@ class UParma(object):
 
         return params_that_trigger_rerun
 
-    def get_default_params(self, target_style):
+    def get_default_params(self, style=None):
         """Fetch translated default params for a given style1.
 
         Args:
-            target_style (str): Translation style, e.g. msfragger_style_3.
+            style (str): Translation style, e.g. msfragger_style_3.
 
         Returns:
             dict: Dict with translated key and translated default value for the given style.
@@ -418,19 +418,19 @@ class UParma(object):
         """
         params = {}
         for key, value in self.parameters.items():
-            translated_key = value["key_translations"].get(target_style, None)
+            translated_key = value["key_translations"].get(style, None)
             if translated_key is not None:
                 untranslated_default = value["default_value"]
                 if (
                     "value_translations" in value
                     and len(value["value_translations"]) > 0
                 ):
-                    if target_style in value["value_translations"]:
+                    if style in value["value_translations"]:
                         translated_default = dict(
-                            value["value_translations"][target_style]
+                            value["value_translations"][style]
                         ).get(
                             untranslated_default,
-                            dict(value["value_translations"][target_style]),
+                            dict(value["value_translations"][style]),
                         )
                     else:
                         translated_default = untranslated_default
