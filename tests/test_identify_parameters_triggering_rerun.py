9922efc194a11b1f8f1089ed7f80af800ab3950a
Christian Fufezan <christian@fufezan.net>
2021-03-30 17:33:32 +0200

    Add test for _identify_parameters_triggering_rerun

---
+++ tests/test_identify_parameters_triggering_rerun.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+import uparma
+import pytest
+
+
+test_data_list = [
+    {
+        "input": {
+            ("general", "parameters"): [
+                {
+                    "_id": 1,
+                    "name": "Luke",
+                    "triggers_rerun": True,
+                    "key_translations": {
+                        "ursgal_style_1": "The force is strong in this o->",
+                    },
+                },
+                {
+                    "_id": 2,
+                    "name": "Leia",
+                    "key_translations": {
+                        "ursgal_style_1": "The force is strong in this o+",
+                    },
+                },
+            ]
+        },
+        "results": ["The force is strong in this o->"],
+    }
+]
+
+
+@pytest.mark.parametrize("test_dict", test_data_list)
+def test_identify_params_trigger_rerun(test_dict):
+    up = uparma.UParma(parameter_data=test_dict["input"])
+    rerun_params = up.identify_parameters_triggering_rerun(
+        [
+            "The force is strong in this o+",
+            "The force is strong in this o->",
+        ]
+    )
+    assert rerun_params == test_dict["results"]
+    rerun_params = up.identify_parameters_triggering_rerun(
+        [
+            "The force is strong in this o+",
+            "The force is strong in this o->",
+        ]
+    )
+    assert rerun_params == test_dict["results"]
