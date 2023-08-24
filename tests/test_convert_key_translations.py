6c9fc4323800bb83e6fe4bccf79ab5de233613fd
YoungKrug <31873199+YoungKrug@users.noreply.github.com>
2023-08-24 11:07:30 -0400

    Added more test cases

--- tests/test_convert_key_translations.py
+++ tests/test_convert_key_translations.py
@@ -1,5 +1,6 @@
 import uparma
 import pytest
+import pprint
 import json
 import os
 from pathlib import Path
@@ -7,6 +8,7 @@ from pathlib import Path
 test_input_dict = {
     "allow_multiple_variable_mods_on_residue": False,
     "mass_diff_to_variable_mod": False,
+    "enzyme_specificity": "full"
 }
 
 test_parameter_list = [
@@ -110,11 +112,13 @@ test_parameter_list = [
         }
     }
 ]
-
 @pytest.mark.parametrize("test_parameters", test_parameter_list)
 def test_load_data(test_parameters):
     up = uparma.UParma(refresh_jsons=False, parameter_data=test_parameter_list[0]["input"])
     translation = up.convert(param_dict=test_input_dict,  translated_style="msfragger_style_3")
+    up_with_parameter_json = uparma.UParma(refresh_jsons=False)
+    translation_params = up_with_parameter_json.convert(param_dict=test_input_dict, translated_style="msfragger_style_3")
+    assert translation_params["enzyme_specificity"]["was_translated"]
     assert translation["allow_multiple_variable_mods_on_residue"]["was_translated"]
     assert translation["allow_multiple_variable_mods_on_residue"]["translated_value"] == 0
     assert translation["write_mass_diff_to_variable_mod"]["was_translated"]
